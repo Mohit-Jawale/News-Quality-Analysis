282222564073d3a787f4fa6e1a77b1a8c8e111f2
prachi jawale <jawale.mohit123@gmail.com>
2020-08-21 21:30:09 +0530

    adding hindu_head

---
+++ hindu_head.py
@@ -0,0 +1,32 @@
+import requests as req
+from bs4 import BeautifulSoup
+
+
+def hindu_head(url):
+        link = url
+        link = req.get(link)
+        soup = BeautifulSoup(link.text, 'html.parser')
+        result = soup.find_all('h1', attrs={'class': "title"})
+        article = []
+        l1 = len(result)
+        flag=0
+        i = 0
+        while i < l1:
+            j = 0
+            exp = result[i]  # this is pne array of paragraph
+            l = len(exp)
+            while j < l:
+                
+                article.append(exp.contents[j].string)
+                j = j+1
+            if flag == 1:
+                flag = 0
+                break
+            i = i+1
+
+        # article.pop(0)
+        article = [x for x in article if x is not None]
+        return article[0]
+
+
+
